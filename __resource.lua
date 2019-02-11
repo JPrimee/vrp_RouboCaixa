d6bc9ba9cc8888d7f0d7990ad4a9d9a153a6af9d
Primeezinho <45419808+JPrimee@users.noreply.github.com>
2019-02-11 01:49:10 -0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+---<<<<### Primezinhoo ###>>>>---
+
+---<<<<### Primezinhoo ###>>>>---
+description "vrp_caixaeletronicos"
+
+dependency "vrp"
+
+client_scripts{ 
+  "client.lua"
+}
+
+server_scripts{ 
+  "@vrp/lib/utils.lua",
+  "server.lua"
+}
